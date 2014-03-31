61ae1e12bbedfa428ee33d1230c86c87efe071ac
Martino <martinojones_2009@hotmail.com>
2014-03-31 18:53:30 -0400

    Update MainScript.ps1
    Added Windows updates ability and added another script to manage Steam

--- MainScript.ps1
+++ MainScript.ps1
@@ -3,53 +3,6 @@
 
 #Check to make sure Steam is installed
 #Loop check
-$i = 0
-do{
-    [boolean]$SteamFileExists = test-path ${env:ProgramFiles(x86)}\Steam\Steam.exe
-    if ($SteamFileExists -eq $True){
-            $i = 1
-        }
-        Else
-        {
-            $SteamErrorPopup = new-object -comobject wscript.shell
-            $SteamErrorAnswer = $SteamErrorPopup.popup(“ERROR! STEAM NOT FOUND! “,0,”STEAM NOT FOUND!”,5)
-
-            #Check answer
-            switch ($SteamErrorAnswer)
-            {
-                1 {}
-                2 {Exit}
-                }
-
-            }
-
-} Until ($i -eq 1)
-
-#Steam Exitsts! Lets start steam
-Start-Process -FilePath ${env:ProgramFiles(x86)}\Steam\Steam.exe
-
-
-#Now that we have steam running, lets get to work on the background stuff
-#Initiate Variables
-$currentPath=Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
-
-#Wirte to file that steam ran and script worked
-$Date =  Get-Date
-$WriteToFile = "Ran correctly at: " + $Date
-$WriteToFile | Out-File $currentPath\ran.txt
-
-#Run Windows Updates
-Get-WindowsUpdate
-
-
-
-
-
-
-
-
-
-
 
 
 #*****************BEING FUNCTIONS*******************#
@@ -73,3 +26,54 @@ Function Get-WindowsUpdate {
         }
     }
 }
+
+
+#Start script
+
+$i = 0
+do{
+    [boolean]$SteamFileExists = test-path ${env:ProgramFiles(x86)}\Steam\Steam.exe
+    if ($SteamFileExists -eq $True){
+            $i = 1
+        }
+        Else
+        {
+            $SteamErrorPopup = new-object -comobject wscript.shell
+            $SteamErrorAnswer = $SteamErrorPopup.popup(“ERROR! STEAM NOT FOUND! “,0,”STEAM NOT FOUND!”,5)
+
+            #Check answer
+            switch ($SteamErrorAnswer)
+            {
+                1 {}
+                2 {Exit}
+                }
+
+            }
+
+} Until ($i -eq 1)
+
+#Steam Exitsts! Lets start steam
+Start-Process Powershell -ArgumentList "-file $currentPath\Managesteam.ps1"
+
+
+#Now that we have steam running, lets get to work on the background stuff
+#Initiate Variables
+$currentPath=Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
+
+#Wirte to file that steam ran and script worked
+$Date =  Get-Date
+$WriteToFile = "Ran correctly at: " + $Date
+$WriteToFile | Out-File $currentPath\ran.txt
+
+#Run Windows Updates
+Get-WindowsUpdate
+
+
+
+
+
+
+
+
+
+
